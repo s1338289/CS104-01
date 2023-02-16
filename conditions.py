52de511410d228540928d1ff6f1b4ab80ac55f75
s1338289 <124280805+s1338289@users.noreply.github.com>
2023-02-16 18:03:29 -0500

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,25 @@
+Python 3.11.1 (v3.11.1:a7a450f84a, Dec  6 2022, 15:24:06) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> temp=int(input("Please Enter the current temperature: "))
+Please Enter the current temperature: 
+============= RESTART: /Users/victordavidson/Documents/entertemp.py ============
+Please Enter the current temperature: 91
+wear shorts
+>>> 72
+72
+>>> 
+============= RESTART: /Users/victordavidson/Documents/entertemp.py ============
+Please Enter the current temperature: 73
+Short Sleeves are fine
+>>> 
+============= RESTART: /Users/victordavidson/Documents/entertemp.py ============
+Please Enter the current temperature: 51
+wear a jacket
+>>> 
+============= RESTART: /Users/victordavidson/Documents/entertemp.py ============
+Please Enter the current temperature: 34
+Wear a heavy coat
+>>> 
+============= RESTART: /Users/victordavidson/Documents/entertemp.py ============
+Please Enter the current temperature: 0
+Stay Inside
